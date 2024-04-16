9c72e389282bb59681722038033ee99772556856
Michele <micheleventurini95@gmail.com>
2024-04-16 14:41:33 +0200

    Button-jquery

--- PROJECT/js/index.js
+++ PROJECT/js/index.js
@@ -1,2 +1,13 @@
+$(document).ready(function(){
+    $(".button-text").hide();
+    $(".button").hover(function(){
+        $(this).find(".button-text1").hide();
+        $(this).find(".button-text").slideDown();
+    }, function() {
+        $(this).find(".button-text").hide();
+        $(this).find(".button-text1").slideDown();
+
+    })
 
 
+})
